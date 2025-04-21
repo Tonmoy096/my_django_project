f0f085b279b7f156baa7725566dcda168dfeb673
Tonmoy096 <foysalkabir2017@gmail.com>
2025-04-22 03:54:15 +0600

    Add models for Tag and Profile, update Product model, and enhance admin interface

--- django_wizard/myapp/admin.py
+++ django_wizard/myapp/admin.py
@@ -5,19 +5,40 @@ admin.site.register(Product)
 # Register your models here.
 '''
 from django.contrib import admin
-from .models import Product, Category
+from .models import Product, Category, Tag, Profile
 # Register your models here.
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id','name', 'price', 'in_stock','category_name')
+    list_display = ('name', 'price','category_name','tag_list')
     search_fields = ('name',)
-    list_filter = ('in_stock',)
-    ordering = ('-id',)
+    list_filter = ('category',)
+    #ordering = ('-id',)
 
     def category_name(self, obj):
         return obj.category.name if obj.category else '-'
     category_name.short_description = 'Category'
 
+    def tag_list(self, obj):
+        return ", ".join([tag.name for tag in obj.tags.all()])
+    tag_list.short_description = 'Tags'
+
+
+
 
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category)
+admin.site.register(Tag)
+admin.site.register(Profile)
+
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+
+class CustomUserAdmin(UserAdmin):
+    inlines = [ProfileInline]
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
--- django_wizard/myapp/models.py
+++ django_wizard/myapp/models.py
@@ -1,17 +1,38 @@
 from django.db import models
-
-class Product(models.Model):
-    name = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits=8, decimal_places=2)
-    in_stock = models.IntegerField()
-    category = models.ForeignKey('category', on_delete=models.CASCADE, null=True, blank=True)
-
-    def __str__(self):
-        return self.name
-# Create your models here.
+from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
+    
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    in_stock = models.IntegerField()
+    category = models.ForeignKey('category', on_delete=models.CASCADE, null=True, blank=True)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.name
+# Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField()
+
+    def __str__(self):
+        return self.user.username
+'''class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name'''
